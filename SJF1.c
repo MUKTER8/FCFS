16298ea0b7a963ab816e192ef81e59bc3482a2f7
MUKTER HOSAIN <mokterprodhan8@gmail.com>
2023-10-02 00:35:03 +0600

    changes

--- SJF1.c
+++ SJF1.c
@@ -1,52 +1,51 @@
 #include <stdio.h>
-int main()
-{
-//initialization
-    int i, j, k = 1, n, temp, p[15], bt[15], at[15], ct[15], tat[15], wt[15];
-    float t_tat = 0, t_wt = 0;
+//initialization varriable
+    int i, j, k = 1, n, tmp, p[15], bt[15], at[15], ct[15], tat[15], wt[15];
+    float atat = 0, awt = 0;
     int min, bt_max = 0;
 
-//getting the process number
-    printf("Enter the number of process---> ");
+void getinput(){
+    
+    printf("Enter the number of process: ");
     scanf("%d", &n);
-    printf("Enter process values of all the process---> ");
+    printf("Enter process : ");
     for (i = 0; i < n; i++)
     {
         scanf("%d", &p[i]);
     }
-//getting brust time and arrival time
-    printf("Enter brust time of all the process---> ");
+    printf("Enter brust time : ");
     for (i = 0; i < n; i++)
     {
         scanf("%d", &bt[i]);
     }
 
-    printf("Enter arrival time of all the process---> ");
+    printf("Enter arrival time : ");
     for (i = 0; i < n; i++)
     {
         scanf("%d", &at[i]);
     }
 
+}
 
+void Different_Arrival_time(){
 
-//positioning
-    for (i = 0; i < n; i++)
+      for (i = 0; i < n; i++)
     {
         for (j = i + 1; j < n; j++)
         {
             if (at[i] > at[j])
             {
-                temp = at[i];
+                tmp = at[i];
                 at[i] = at[j];
-                at[j] = temp;
+                at[j] = tmp;
 
-                temp = bt[i];
+                tmp = bt[i];
                 bt[i] = bt[j];
-                bt[j] = temp;
+                bt[j] = tmp;
 
-                temp = p[i];
+                tmp = p[i];
                 p[i] = p[j];
-                p[j] = temp;
+                p[j] = tmp;
             }
         }
     }
@@ -59,45 +58,105 @@ int main()
         {
             if (bt_max >= at[j] && bt[j] < min)
             {
-                temp = p[i + 1];
+                tmp = p[i + 1];
                 p[i + 1] = p[j];
-                p[j] = temp;
+                p[j] = tmp;
 
-                temp = at[i + 1];
+                tmp = at[i + 1];
                 at[i + 1] = at[j];
-                at[j] = temp;
+                at[j] = tmp;
 
-                temp = bt[i + 1];
+                tmp = bt[i + 1];
                 bt[i + 1] = bt[j];
-                bt[j] = temp;
+                bt[j] = tmp;
             }
         }
         k++;
     }
 
 
-//determination of  ct,tat,wt,total_tat,total_wt
     ct[-1] = 0;
     for (i = 0; i < n; i++)
     {
         ct[i] = bt[i] + ct[i - 1];
         tat[i] = ct[i] - at[i];
+        atat = atat + tat[i];
         wt[i] = tat[i] - bt[i];
-        t_tat = t_tat + tat[i];
-        t_wt = t_wt + wt[i];
+        awt = awt + wt[i];
     }
 
-    printf("\n-------------|---------------|---------------|---------------|---------------|--------------\n");
+}
 
-    printf("Process\t\tBT\t\tAT\t\tCT\t\tTAT\t\tWT\n");
+void Same_arrival_time(){
+     
+     for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(bt[i]>bt[j]){
+                
+                tmp=bt[i];
+                bt[i]=bt[j];
+                bt[j]=tmp;
+
+                tmp=p[i];
+                p[i]=p[j];
+                p[j]=tmp;
+
+            }
+        }
+    }
+    int sum=0;
+    for(i=0;i<n;i++){
+        sum+=bt[i];
+        ct[i]+=sum;
+    }
+    for(i=0;i<n;i++){
+        tat[i]=ct[i]-at[i];
+        atat=atat+tat[i];
+        wt[i]=tat[i]-bt[i];
+        awt=awt+wt[i];
+    }
+}
+void printfunction(){
+     printf("Process\t\tBT\t\tAT\t\tTAT\t\tWT\n");
 
     for (i = 0; i < n; i++)
     {
-        printf("P[%d]\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", p[i], bt[i], at[i], ct[i], tat[i], wt[i]);
+        printf("P[%d]\t\t%d\t\t%d\t\t%d\t\t%d\n", p[i], bt[i], at[i], tat[i], wt[i]);
     }
-            printf("-------------|---------------|---------------|---------------|---------------|--------------\n");
 
 
-    printf("\t\t\t\t    AvgTAT : %f    \n", (t_tat / n));
-    printf("\t\t\t\t    AvgWT  : %f    \n", (t_wt / n));
+    printf("Average turnaround time : %.2f \n", (atat / n));
+    printf("Average waiting time    : %.2f \n", (awt / n));
 }
+
+int main()
+{
+int ch;
+do{
+    printf("Press 1:SAME ARRIVAL TIME\n");
+    printf("Press 2 :Different Arrival time\n");
+    printf("Press any key :Exit the program\n");
+    printf("Enter your choice : ");
+    scanf("%d",&ch);
+    switch (ch)
+    {
+    case 1:
+    getinput();
+    Same_arrival_time();
+    printfunction();
+        break;
+    case 2:
+    getinput();
+    Different_Arrival_time();
+    printfunction();
+    
+    default:
+    printf("\nExit the program.");
+        break;
+    }
+}while (ch!=-1);
+
+}
+
+
+   
